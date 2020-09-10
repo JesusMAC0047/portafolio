8fed2a2e6f285404e47bcddaec5fdc578daff7a0
jesusMAC0047 <jesus0047_mac@outlook.com>
2020-09-09 23:53:59 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ErrorComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
