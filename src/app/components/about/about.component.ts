641dedadeee3a20f06f81c884efb37dd0f41d943
jesusMAC0047 <jesus0047_mac@outlook.com>
2020-09-14 22:30:10 -0500

    se termino el area sobre mi

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   
+  title: string;
+  paraghap: string;
+  
   constructor() {
-    
+    this.title = "Sobre mí";
+    this.paraghap = `
+      Buen día, les cuento un poco sobre mí, hace poco tiempo termine la universidad con la 
+      licenciatura de sistemas computacionales, en este portafolio les dare a conozer los
+      lenguajes y frameworks que se manejar y un poco de proyectos que he desarrollado para 
+      poner a prueba mis conocimientos.
+    `;
   }
 
   ngOnInit(): void {
