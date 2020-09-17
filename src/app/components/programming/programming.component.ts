a2328e90f7dcf3903827cd8580c881e1d5286812
jesusMAC0047 <jesus0047_mac@outlook.com>
2020-09-16 22:14:21 -0500

    se termino el area de programacion

--- src/app/components/programming/programming.component.ts
+++ src/app/components/programming/programming.component.ts
@@ -7,7 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgrammingComponent implements OnInit {
 
-  constructor() { }
+  lenguajes: string[];
+  frameworks: string[];
+  dataBases: string[];
+  enmaquetados: string[];
+  otros: string[];
+
+  constructor() {
+  
+    this.lenguajes = [
+      'PHP',
+      'JavaScript',
+      'TypeScript',
+      'Java'
+    ];
+    
+    this.frameworks = [
+      'Laravel',
+      'Angular',
+      'JQuery',
+      'Node.js'
+    ];
+    
+    this.dataBases = [
+      'MariaDB',
+      'MongoDB'
+    ];
+    
+    this.enmaquetados = [
+      'CSS',
+      'HTML',
+      'Bootstrap 4'
+    ];
+    
+    this.otros = [
+      'Github',
+      'Programación orientada a objetos',
+      'Modelo vista controlador'
+    ];
+  }
 
   ngOnInit(): void {
   }
