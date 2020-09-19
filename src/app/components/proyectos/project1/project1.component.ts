2cc299abfa20ab3a4c0d383236641fea8f3c9857
jesusMAC0047 <jesus0047_mac@outlook.com>
2020-09-19 17:45:34 -0500

    enmaquetado de la informacion de los proyectos

--- src/app/components/proyectos/project1/project1.component.ts
+++ src/app/components/proyectos/project1/project1.component.ts
@@ -10,14 +10,23 @@ export class Project1Component implements OnInit {
   title: string;
   descripcion: string;
   list: string [];
+  github: string;
 
   constructor() { }
 
   ngOnInit(): void {
   
-  this.title = "Portafolio";
+    this.title = "Portafolio";
   
-  this.list = ['Angular', 'JQuery', 'Typescript', 'Bootstrap 4', 'HTML', 'CSS', 'Moment js'];
+    this.list = ['Angular', 'JQuery', 'Typescript', 'Bootstrap 4', 'HTML', 'CSS', 'Moment js'];
+    
+    this.github = "Ver repositorio de este proyecto en GitHub"
+  
+    this.descripcion = `
+      Este portafolio lo cree para mostrar los proyectos que he realizado al momento y los que posteriormente
+      vaya realizando en un futuro. Con este portafolio me quiero dar a conozer compartiendo el repositorio
+      de los proyectos para dar a conozer las tecnologias que se manejar hasta el momento.
+    `;
   }
 
 }
--- src/app/components/proyectos/project2/project2.component.ts
+++ src/app/components/proyectos/project2/project2.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Project2Component implements OnInit {
 
+  title: string;
+  descripcion: string;
+  list: string [];
+  github: string;
+  
   constructor() { }
 
   ngOnInit(): void {
+  
+    this.title = "Peliculas";
+  
+    this.list = ['HTML', 'CSS', 'PHP', 'SQL'];
+    
+    this.github = "Ver repositorio de este proyecto en GitHub"
+  
+    this.descripcion = `
+      Este proyecto fue el primero que hize para incluir todos mis conocimientos en un proyecto grande, fue creado
+      como un proyecto para mi ultimo cuatrimestre en la universidad donde se tenia que incluir una base
+      de datos, decidi usar solamente PHP sin ningun framwork 
+    `;
   }
 
 }
